508334d41976bcbcae843a5c80af1f42bf8c0e06
Michael Calkins <m.calkins576@gmail.com>
2019-11-13 17:25:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(arr)
+    arr.each do |x|
+      master = x.split("")
+    
+    end
+    
+  end
+  
+  
+  
+  
+end
